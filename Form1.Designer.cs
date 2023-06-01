6d6f0326891f7d85a171303f45ba01fba4d2047c
Gustavo Quim <gquimb@miumg.edu.gt>
2023-05-31 22:28:52 -0600

    Subida de archivos base

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CalculadoraIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.TxtPESO = new System.Windows.Forms.TextBox();
+            this.LblPeso = new System.Windows.Forms.Label();
+            this.TxtAltura = new System.Windows.Forms.TextBox();
+            this.LblAltura = new System.Windows.Forms.Label();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            this.LblEdad = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BtnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalcular.Location = new System.Drawing.Point(364, 285);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(155, 62);
+            this.BtnCalcular.TabIndex = 15;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = false;
+            // 
+            // TxtPESO
+            // 
+            this.TxtPESO.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPESO.Location = new System.Drawing.Point(365, 210);
+            this.TxtPESO.Name = "TxtPESO";
+            this.TxtPESO.Size = new System.Drawing.Size(97, 29);
+            this.TxtPESO.TabIndex = 14;
+            // 
+            // LblPeso
+            // 
+            this.LblPeso.AutoSize = true;
+            this.LblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPeso.Location = new System.Drawing.Point(282, 212);
+            this.LblPeso.Name = "LblPeso";
+            this.LblPeso.Size = new System.Drawing.Size(57, 24);
+            this.LblPeso.TabIndex = 13;
+            this.LblPeso.Text = "Peso";
+            // 
+            // TxtAltura
+            // 
+            this.TxtAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtAltura.Location = new System.Drawing.Point(365, 157);
+            this.TxtAltura.Name = "TxtAltura";
+            this.TxtAltura.Size = new System.Drawing.Size(97, 29);
+            this.TxtAltura.TabIndex = 12;
+            // 
+            // LblAltura
+            // 
+            this.LblAltura.AutoSize = true;
+            this.LblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAltura.Location = new System.Drawing.Point(282, 159);
+            this.LblAltura.Name = "LblAltura";
+            this.LblAltura.Size = new System.Drawing.Size(64, 24);
+            this.LblAltura.TabIndex = 11;
+            this.LblAltura.Text = "Altura";
+            // 
+            // TxtEdad
+            // 
+            this.TxtEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtEdad.Location = new System.Drawing.Point(365, 103);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(97, 29);
+            this.TxtEdad.TabIndex = 10;
+            // 
+            // LblEdad
+            // 
+            this.LblEdad.AutoSize = true;
+            this.LblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEdad.Location = new System.Drawing.Point(282, 105);
+            this.LblEdad.Name = "LblEdad";
+            this.LblEdad.Size = new System.Drawing.Size(59, 24);
+            this.LblEdad.TabIndex = 9;
+            this.LblEdad.Text = "Edad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.TxtPESO);
+            this.Controls.Add(this.LblPeso);
+            this.Controls.Add(this.TxtAltura);
+            this.Controls.Add(this.LblAltura);
+            this.Controls.Add(this.TxtEdad);
+            this.Controls.Add(this.LblEdad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.TextBox TxtPESO;
+        private System.Windows.Forms.Label LblPeso;
+        private System.Windows.Forms.TextBox TxtAltura;
+        private System.Windows.Forms.Label LblAltura;
+        private System.Windows.Forms.TextBox TxtEdad;
+        private System.Windows.Forms.Label LblEdad;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CalculadoraIMC
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnGuardadr = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblIndicador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(521, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 62);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Ver plan nutricional";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // BtnGuardadr
+            // 
+            this.BtnGuardadr.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGuardadr.Location = new System.Drawing.Point(142, 331);
+            this.BtnGuardadr.Name = "BtnGuardadr";
+            this.BtnGuardadr.Size = new System.Drawing.Size(150, 62);
+            this.BtnGuardadr.TabIndex = 16;
+            this.BtnGuardadr.Text = "Guardar Datos";
+            this.BtnGuardadr.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(127, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 24);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Persona";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(504, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 24);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Obesidad Grado ll";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(551, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 24);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Grafica";
+            // 
+            // LblIndicador
+            // 
+            this.LblIndicador.AutoSize = true;
+            this.LblIndicador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblIndicador.Location = new System.Drawing.Point(567, 72);
+            this.LblIndicador.Name = "LblIndicador";
+            this.LblIndicador.Size = new System.Drawing.Size(45, 24);
+            this.LblIndicador.TabIndex = 12;
+            this.LblIndicador.Text = "IBM";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnGuardadr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblIndicador);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnGuardadr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblIndicador;
+    }
+}
