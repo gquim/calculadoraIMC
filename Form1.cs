371293b5de016a8fb86856d4883cd083effd37d5
Adolfo Cantoral <pcantorall@miumg.edu.gt>
2023-06-01 11:52:45 -0600

    Mejora de interfaz

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,6 +18,12 @@ namespace CalculadoraIMC
         {
             InitializeComponent();
         }
+        //*********Permite mover el login con mause
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+        // ************************************************
 
         private void FrmPrincipal_Load(object sender, EventArgs e)
         {
@@ -27,6 +34,22 @@ namespace CalculadoraIMC
         {
             
 
+        }
+
+        private void FrmPrincipal_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void BtnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
